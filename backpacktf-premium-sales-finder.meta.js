deecf9dc37a1dd62a3d0101279931cac39b1dcba
yaboieeek <typarasa@gmail.com>
2025-11-20 20:01:48 +0300

    Modify metadata for backpack.tf Premium Sales Finder
    Updated metadata to reflect new author and functionality.

--- backpacktf-premium-sales-finder.meta.js
+++ backpacktf-premium-sales-finder.meta.js
@@ -1,14 +1,16 @@
 // ==UserScript==
-// @name        backpack.tf Premium Recent Sales Finder
-// @namespace   http://steamcommunity.com/profiles/76561198080179568/
-// @version     4.1.1
-// @description Adds coloring to history pages indicating recent sales and includes compare links for sales
-// @author      Julia
-// @updateURL   https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.meta.js
-// @downloadURL https://github.com/juliarose/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.user.js
+// @name        backpack.tf Premium Recent Sales Finder + indate sales toggler
+// @namespace   http://steamcommunity.com/profiles/76561198080179568/ and https://steamcommunity.com/profiles/76561198967088046
+// @version     4.1.1.fork.1
+// @description Adds coloring to history pages indicating recent sales and includes compare links for sales + adds sales toggler
+// @author      Julia and eeek
+// @updateURL   https://github.com/yaboieeek/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.meta.js
+// @downloadURL https://github.com/yaboieeek/backpack.tf-premium-sales-finder/raw/master/backpacktf-premium-sales-finder.user.js
 // @run-at      document-end
 // @grant       GM_addStyle
 // @grant       unsafeWindow
+// @grant       GM_getValue
+// @grant       GM_setValue
 // @include     /^https?:\/\/(.*\.)?backpack\.tf(:\d+)?\/item\/\d+/
 // @include     /^https?:\/\/(.*\.)?backpack\.tf(:\d+)?\/premium\/search.*/
 // @include     /^https?:\/\/(.*\.)?backpack\.tf\/profiles\/\d{17}/
